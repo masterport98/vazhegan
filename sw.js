2d4e35d33fa2ab1fad2b7c3a0ac2b6d9dc8597e1
masterport98 <masterport@gmail.com>
2026-09-20 07:54:25 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ganjineh-v1';
+const CACHE = 'ganjineh-0034cad9';
 const CORE = ['./', './index.html', './manifest.webmanifest',
               './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
